58d96e57dc8aab2b3779b2a7669d3fead7f61470
Dimitri Gedda <gratitudeadore@fastmail.com>
2020-06-18 00:44:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const createFareMultiplier = function(multiplier){
 const fareDoubler = createFareMultiplier(2)
 const fareTripler = createFareMultiplier(3)
   
-}
+
 function selectDifferentDrivers(){
   
 }
